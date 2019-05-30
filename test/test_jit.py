a676dd33c4d0b2b60c3e9ce725bda0abeb22375f
Horace He <chilli@fb.com>
2019-05-30 13:53:07 -0700

    Added factorial
    gh-metadata: pytorch pytorch 21129 gh/chillee/7/head

--- test/test_jit.py
+++ test/test_jit.py
@@ -6125,7 +6125,7 @@ a")
                            "asinh", "atanh", "acosh", "sinh", "cosh", "tanh"]
         binary_float_ops = ["atan2", "fmod", "copysign"]
         for op in unary_float_ops:
-            checkMathWrap(op)
+            checkMathWrap(op, 1)
         for op in binary_float_ops:
             checkMathWrap(op, 2)
 
@@ -6138,6 +6138,7 @@ a")
             checkMathWrap("gcd", 2, is_float=False, ret_type="int")
         if PY37:
             checkMathWrap("remainder", 2)
+        checkMathWrap("factorial", 1, is_float=False, ret_type="int", vals=[(i, i) for i in range(-2, 10)])
 
     @unittest.skipIf(PY2, "Requires python 3")
     def test_math_gcd(self):
